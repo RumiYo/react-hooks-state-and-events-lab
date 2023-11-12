798eddf84158d8c1eb798432c827bdb7c4d027f8
RumiYo <yokoyamarumi@gmail.com>
2023-11-12 14:39:02 -0500

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
@@ -6,13 +6,30 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  //const appClass = false ? "App dark" : "App light"
+
+  const [appClass, setAppClass] = useState("App light")
+  const [appColor, setAppColor] = useState("Light Mode")
+
+console.log(appClass);
+
+  function changeAppClass(event){
+    if(appClass==="App dark"){
+      setAppClass("App light");
+      setAppColor("Light Mode");
+    }else{
+      setAppClass("App dark");
+      setAppColor("Dark Mode")
+    }
+  }; 
+  
+
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={changeAppClass}>{appColor}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [className, setClassName] = useState("");
+  const [buttonText, setButtonText] = useState("Add to Cart");
+
+  function changeCartStatus(){
+    console.log('clicked')
+    if(className===""){
+      setClassName("in-cart")
+      setButtonText("Remove From Cart")
+    }else{
+      setClassName("")
+      setButtonText("Add to Cart")
+    }
+  }
+
   return (
-    <li className="">
+    <li className={className}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={changeCartStatus}>{buttonText}</button>
     </li>
   );
 }
